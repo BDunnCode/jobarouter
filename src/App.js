9ba42abafa02b84271cbf34c362107715a6d3ab0
BDunnCode <BDunnCode@github.com>
2024-01-05 00:36:59 -0600

    final commit for day

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Jobarouter</h1>
+          <Link to="/"></Link>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
