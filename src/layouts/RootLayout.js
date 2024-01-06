84ea69bed6cc8c40fd7d63e39f133dfc34b893db
BDunnCode <BDunnCode@github.com>
2024-01-06 16:22:45 -0600

    added breadcrumbs functionality, started new forms

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,4 +1,5 @@
 import { NavLink, Outlet } from "react-router-dom"
+import Breadcrumbs from "../components/Breadcrumbs"
 
 export default function RootLayout() {
   return (
@@ -11,7 +12,9 @@ export default function RootLayout() {
           <NavLink to="help">Help</NavLink>
           <NavLink to="careers">Careers</NavLink>
         </nav>
+        <Breadcrumbs />
       </header>
+
       <main>
         <Outlet />
       </main>
